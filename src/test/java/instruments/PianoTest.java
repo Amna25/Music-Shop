bc5b21aad10d441af0c7840b1ed83af4d0d8f767
Amna <amna.bashir2@gmail.com>
2022-02-18 13:53:17 +0000

    create ISell interface

--- src/test/java/instruments/PianoTest.java
+++ src/test/java/instruments/PianoTest.java
@@ -1,5 +1,38 @@
 package instruments;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PianoTest {
-    
+    Piano piano;
+
+    @Before
+    public void before(){
+        piano = new Piano("Pop Rock","Purple", "Parlor Grand", 88);
+    }
+
+    @Test
+    public void hasStyle(){
+        assertEquals("Pop Rock", piano.getStyle());
+    }
+    @Test
+    public void hasColour(){
+        assertEquals("Purple",piano.getColour());
+    }
+    @Test
+    public void hasType(){
+        assertEquals("Parlor Grand", piano.getType());
+    }
+    @Test
+    public void hasNumberOfDrums(){
+        assertEquals(88, piano.getNumOfKeys());
+    }
+
+    @Test
+    public void canPlaySound(){
+        assertEquals("tone tone na na na", piano.play());
+    }
+
 }
