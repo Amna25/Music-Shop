2bd43e33cc54adbd823823f98dd4d80c98a02b4b
Amna <amna.bashir2@gmail.com>
2022-02-18 13:37:17 +0000

    guitar test all passes

--- src/test/java/instruments/GuitarTest.java
+++ src/test/java/instruments/GuitarTest.java
@@ -1,4 +1,42 @@
 package instruments;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuitarTest {
+    Guitar guitar;
+
+    @Before
+    public void before(){
+        guitar = new Guitar("Jazz", "Blue","Electric", 6,2);
+    }
+
+
+    @Test
+    public void hasStyle(){
+        assertEquals("Jazz", guitar.getStyle());
+    }
+    @Test
+    public void hasColour(){
+        assertEquals("Blue", guitar.getColour());
+    }
+    @Test
+    public void hasType(){
+        assertEquals("Electric", guitar.getType());
+    }
+    @Test
+    public void hasNumberOfDrums(){
+        assertEquals(6, guitar.getNumOfStrings());
+    }
+    @Test
+    public void hasNumberOfSticks(){
+        assertEquals(2, guitar.getToneKnobs());
+    }
+    @Test
+    public void canPlaySound(){
+        assertEquals("a-woogah woogah", guitar.play());
+    }
+
 }
