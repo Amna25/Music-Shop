7b965f4296650b24a2f861e11bcdc8c023512cbc
Amna <amna.bashir2@gmail.com>
2022-02-18 14:42:06 +0000

    Accessories item test done

--- src/test/java/items/AccessoriesTest.java
+++ src/test/java/items/AccessoriesTest.java
@@ -1,4 +1,33 @@
 package items;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class AccessoriesTest {
+    Accessories accessories;
+
+    @Before
+    public void before(){
+        accessories = new Accessories("MicroPhone", 200,100);
+    }
+
+    @Test
+    public void hasItem(){
+        assertEquals("Microphone", accessories.getItem());
+    }
+    @Test
+    public void hasSellingPrice(){
+        assertEquals(200, accessories.getSellingPrice(),0.01);
+    }
+
+    @Test
+    public void hasBuyingPrice(){
+        assertEquals(100, accessories.getBuyingPrice(), 0.01);
+    }
+    @Test
+    public void canCalculateMarkup(){
+        assertEquals(200, accessories.calculateMarkup(), 0.01);
+    }
 }
