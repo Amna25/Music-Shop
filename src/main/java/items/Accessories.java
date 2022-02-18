92e296e6b8c56e37eeb746155a4effa4c8959a97
Amna <amna.bashir2@gmail.com>
2022-02-18 14:36:15 +0000

    create accessories class

--- src/main/java/items/Accessories.java
+++ src/main/java/items/Accessories.java
@@ -1,4 +1,32 @@
 package items;
 
-public class Accessories {
+import behaviours.ISell;
+
+public class Accessories implements ISell {
+    private String item;
+    private double sellingPrice;
+    private double buyingPrice;
+
+    public Accessories(String item, double sellingPrice, double buyingPrice) {
+        this.item = item;
+        this.sellingPrice = sellingPrice;
+        this.buyingPrice = buyingPrice;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public double getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public double getBuyingPrice() {
+        return buyingPrice;
+    }
+
+    @Override
+    public double calculateMarkup() {
+        return sellingPrice;
+    }
 }
